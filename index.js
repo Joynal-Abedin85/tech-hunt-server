9d272f478ccaf92069f524e0b51a708c90e924b7
kazi tareq <kazitareq321@gmail.com>
2025-01-21 01:44:38 +0600

    add user and products api

---
+++ index.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+
+const port = process.env.PORT || 5000;
+
+
+app.use(cors())
+app.use(express.json())
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c3mzl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const techcollection = client.db("tech-hub").collection('tech')
+    const usercollection = client.db('tech-hub').collection('users')
+
+
+    app.post('/jwt', async(req,res)=> {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: '10h'
+        })
+        res.send({token})
+      })
+
+
+    // tech api 
+
+    app.post('/tech',async (req,res)=> {
+        const item = req.body;
+        const result = await techcollection.insertOne(item)
+        res.send(result)
+      })
+
+
+    app.post('/users',  async(req,res)=> {
+        const user = req.body
+  
+        const query = {email: user.email}
+        const existuser = await usercollection.findOne(query)
+        if(existuser){
+          return res.send({message: 'user already exist', insertedId: null})
+        }
+  
+        const result = await usercollection.insertOne(user)
+        res.send(result)
+      })
+  
+      app.get('/users', async(req,res)=> {
+        
+        const result = await usercollection.find().toArray()
+        res.send(result)
+      })
+
+      app.get('/users/admin/:email',  async (req,res) => {
+        const email = req.params.email;
+        if ( email !== req.decoded.email){
+          return res.status(403).send({message: 'unauthorized access'})
+        }
+  
+        const query = {email: email}
+        const user = await usercollection.findOne(query)
+        let admin = false;
+        if (user) {
+          admin = user?.role === 'admin'
+        }
+        res.send({admin})
+      })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req,res) => {
+    res.send('tech is running')
+})
+
+app.listen(port, ()=> {
+    console.log('tech is run port ')
+})
