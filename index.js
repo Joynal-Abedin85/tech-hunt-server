36f0d9ee821e72047c4dbde3e5c4c247afe9479b
kazi tareq <kazitareq321@gmail.com>
2025-01-26 12:03:18 +0600

    user delete api

--- index.js
+++ index.js
@@ -221,6 +221,13 @@ async function run() {
 
     // user api
 
+    app.delete('/users/:id', async (req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await usercollection.deleteOne(query)
+      res.send(result)
+    })
+
     app.post("/users", async (req, res) => {
       const user = req.body;
 
