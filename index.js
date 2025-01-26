fe46894a887971a1be71369270c3083801fac124
kazi tareq <kazitareq321@gmail.com>
2025-01-26 13:37:46 +0600

    delete api

--- index.js
+++ index.js
@@ -117,6 +117,14 @@ async function run() {
     });
 
 
+    app.delete('/tech/:id', async(req,res)=> {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await techcollection.deleteOne(query)
+      res.send(result)
+    })
+
+
 
 
 
